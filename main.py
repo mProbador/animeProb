ff93b44f0a348cd86e47e856d46fe5f93a545103
mProbador <130259859+mProbador@users.noreply.github.com>
2023-04-08 20:10:10 -0500

    active la grafiac BAR

--- main.py
+++ main.py
@@ -9,3 +9,4 @@ if __name__ == '__main__':
     data = read_anime.ani_csv('./my_proyect2/anime.csv')
     animes, ratings = utils.get_rating_ani(data)
     charts.generate_pie_chart(animes, ratings)
+    charts.generat_bar_chart(animes, ratings)
