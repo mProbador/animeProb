205d7353ae4b74dfb9726566f61b87875ee3857a
Can <100708595+imLost14@users.noreply.github.com>
2023-04-09 12:33:04 -0500

    quite una grafica
    no es necesaria

--- main.py
+++ main.py
@@ -8,5 +8,6 @@ import collections
 if __name__ == '__main__':
     data = read_anime.ani_csv('./my_proyect2/anime.csv')
     animes, ratings = utils.get_rating_ani(data)
-    charts.generate_pie_chart(animes, ratings)
+    #charts.generate_pie_chart(animes, ratings)
     charts.generat_bar_chart(animes, ratings)
+    # No necesitas la grafica de pie, puesto que no muestra bien las diferrencias
